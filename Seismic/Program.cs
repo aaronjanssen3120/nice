09bb93a6af8d54a12934011f0724e51cdcd9ad49
aaronjanssen3120 <aaronjanssen3120@gmail.com>
2011-12-20 23:27:47 -0600

    thats all for tonight

--- Seismic/Program.cs
+++ Seismic/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 // use the Sho libraries:
 using ShoNS.Array;
 using ShoNS.Visualization;
@@ -9,27 +10,50 @@ using ShoNS.MathFunc;  // for the RandomDoubleArray method
 using ShoNS.SignalProc;
 using ShoNS.Utils;
 using ShoNS.Stats;
+
 namespace Seismic
 {
-    // Class definition.
-    public class MyCustomClass
+        /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class TimeSeries
     {
-        // Class members:
-        // Property.
-        public int Number { get; set; }
+        /// <summary>
+        /// The x values
+        /// </summary>
+        public DoubleArray x;
+        /// <summary>
+        /// The y values
+        /// </summary>
+        public DoubleArray y;
 
-        // Method.
-        public int Multiply(int num)
+        public TimeSeries(DoubleArray x, DoubleArray y)
         {
-            return num * Number;
+             //DoubleArray x = x;
+             //DoubleArray y=y;
         }
 
-        // Instance Constructor.
-        public MyCustomClass()
+        public void plot()
         {
-            Number   = 0;
+             IFigure f2 = ShoPlotHelper.Figure();
+             DoubleArray pme = ArrayRandom.RandomDoubleArray(50,50);
+             ArrayImage.GetArrayImage(pme,-1.0,1.0);
+           ShoChart ch = new ShoContourChart(pme, null);
+
+             
         }
-    }
+
+
+
+
+
+        }
+
+
+
+
+    
+    
     // Another class definition. This one contains
     // the Main method, the entry point for the program.
     class Program
@@ -38,7 +62,21 @@ namespace Seismic
         {
             System.Console.WriteLine("Hello, World!");
 
-            
+                        DoubleArray ee = ArrayRandom.RandomDoubleArray(4096);
+            DoubleArray ef = ArrayRandom.RandomDoubleArray(4096);
+
+            TimeSeries myTimeSeries = new TimeSeries(ef,ee);
+
+            TimeSeries myOtherTimeSeries = myTimeSeries;
+
+            myTimeSeries.x = ef;
+
+            myTimeSeries.plot();
+
+            System.Console.WriteLine(myOtherTimeSeries.x);
+
+
+
            // IFigure f2 = ShoPlotHelper.Figure();
 
             System.Console.WriteLine("Starting Slow");
@@ -58,10 +96,10 @@ namespace Seismic
             System.Console.WriteLine("Starting Fast");
             System.Console.WriteLine(DateTime.Now);
             ArraySettings.EnableFastMath();
+
+            CubicSpline cs = new CubicSpline();
             DoubleArray d = ArrayRandom.RandomDoubleArray(4096);
             DoubleArray e = ArrayRandom.RandomDoubleArray(4096);
-            CubicSpline cs = new CubicSpline();
-            d = ArrayRandom.RandomDoubleArray(4096);
             DoubleArray g = d.Sort(); cs.Fit(g, e);
             DoubleArray h = cs.Interp(g);
             for (int i = 0; i < 10000; i++)
@@ -69,7 +107,7 @@ namespace Seismic
                 //DoubleArray f = ConvComp.Conv(d, e);
 
           
-                cs.Fit(g, e);
+                //cs.Fit(g, e);
                  h = cs.Interp(g);
             }
 
